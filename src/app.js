52090757b71c0083d2eb60d70b9af8b38710ed4a
Sebastian Vega <svegalopez@gmail.com>
2017-08-13 20:59:39 -0400

    entry point (app.js) complete

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 var newTrack = require('./eventListeners/new_track');
 var configBlock = require('./eventListeners/config_block');
+var transport = require('./eventListeners/transport');
+var sessions = require('./eventListeners/sessions');
 
-const init = () => {
+const init = () => {  //initializes the entire application...
 
     console.log('JS Started!');
     newTrack.startListening();
     configBlock.startListening();
+    transport.startListening();
+    sessions.startListening();
+
+    // More features to be added...
 
 }
 
+/////////////////...THIS IS WHAT  RUNS...//////////////////// 
 
-
-// THIS IS WHAT  RUNS...
 window.addEventListener('load', init);
